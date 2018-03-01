d55857c35139731e789b8897e8a63c4a148df9aa
marmothetka <golenok-ea5@narod.ru>
2018-03-01 15:14:18 +0300

    add AsParallelDeadlock Demo and delete bin

---
+++ AsParallelDeadlock/AsParallelDeadlock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace AsParallelDeadlock
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Execute(new ManualResetEventSlim());
+        }
+
+        static void Execute(ManualResetEventSlim mre)
+        {
+            Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().Id);
+            Enumerable.Range(0, Environment.ProcessorCount*100).AsParallel()
+                .ForAll(j =>
+                {
+                    if (j == Environment.ProcessorCount)
+                    {
+                        Console.WriteLine("Set on {0} with value of {1}", Thread.CurrentThread.ManagedThreadId, j);
+                        mre.Set();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Waiting on {0} with value of {1}", Thread.CurrentThread.ManagedThreadId, j);
+                        mre.Wait();
+                    }
+                });
+        }
+    }
+}
